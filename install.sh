552676b8b60bb32bf4e96c4b096a36fce77eb239
王明坤 <wangmingkun239@lu.com>
2019-06-21 15:12:01 +0800

    use `xcode-select -p` to locate xcode dir

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-
+XCODE_DEV=$(xcode-select -p)
 DES_DIRS=(
-'/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/'
-'/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/'
-'/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/'
-'/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/'
+'Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/'
+'Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/'
+'Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/'
+'Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/'
 )
 for ((i=0; i<${#DES_DIRS[@]}; ++i));do
     src=$((i+1))
-    des=${DES_DIRS[$i]}
+    des="${XCODE_DEV}/${DES_DIRS[$i]}"
     echo "cp lib from $src to $des"
     cp -iR $src/* $des
 done
+
+exit 0
